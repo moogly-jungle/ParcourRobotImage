28f5bed46cdf3882cc13142da571f59aa562c43a
Olivier Ly <ly.olivier@gmail.com>
2022-02-28 13:19:48 +0100

    ajout des enoncés

---
+++ tp3.py
@@ -0,0 +1,182 @@
+## Cas d'étude classique: les chien et chats
+
+# Préliminaire: téléchargement des données:
+# > curl -O https://download.microsoft.com/download/3/E/1/3E1C3F21-ECDB-4869-8368-6DEBA77B919F/kagglecatsanddogs_3367a.zip
+# > unzip -q kagglecatsanddogs_3367a.zip
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import os
+import sys
+
+### Suppression des données mal encodées.
+num_skipped = 0
+for folder_name in ("Cat", "Dog"):
+    folder_path = os.path.join("PetImages", folder_name)
+    for fname in os.listdir(folder_path):
+        fpath = os.path.join(folder_path, fname)
+        try:
+            fobj = open(fpath, "rb")
+            is_jfif = tf.compat.as_bytes("JFIF") in fobj.peek(10)
+        finally:
+            fobj.close()
+
+        if not is_jfif:
+            num_skipped += 1
+            # Delete corrupted image
+            os.remove(fpath)
+
+
+
+## Génération des données d'entrainement et de validataion
+image_size = (180, 180)
+batch_size = 32
+
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    "PetImages",
+    validation_split=0.2,
+    subset="training",
+    seed=1337,
+    image_size=image_size,
+    batch_size=batch_size,
+)
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    "PetImages",
+    validation_split=0.2,
+    subset="validation",
+    seed=1337,
+    image_size=image_size,
+    batch_size=batch_size,
+)
+
+## QUESTION: quel sont les types de train_ds et de val_ds ? En utilisant par exemple train_ds.as_numpy_iterator(), explorez et interprétez le format du premier élément de train_ds
+
+## Exploration manuelle: visualisation des données
+import matplotlib.pyplot as plt
+glb_visu = False
+if glb_visu:
+    plt.figure(figsize=(10, 10))
+    for images, labels in train_ds.take(1):
+        for i in range(9):
+            ax = plt.subplot(3, 3, i + 1)
+            plt.imshow(images[i].numpy().astype("uint8"))
+            plt.title(int(labels[i]))
+            plt.axis("off")
+    plt.show()
+
+## data augmentation
+data_augmentation = keras.Sequential(
+    [
+        layers.experimental.preprocessing.RandomFlip("horizontal"),
+        layers.experimental.preprocessing.RandomRotation(0.1),
+    ]
+)
+
+## QUESTION: Avec le code suivant, visualisez les images "augmentées", que s'est-il passé ?
+augment_visu = False
+if augment_visu:
+    plt.figure(figsize=(10, 10))
+    for images, _ in train_ds.take(1):
+        for i in range(9):
+            augmented_images = data_augmentation(images)
+            ax = plt.subplot(3, 3, i + 1)
+            plt.imshow(augmented_images[0].numpy().astype("uint8"))
+            plt.axis("off")
+    plt.show()
+
+## QUESTION: à quoi sert l'augmentation de donnée ?
+
+## Optimisation d'accès aux fichiers
+train_ds = train_ds.prefetch(buffer_size=32)
+val_ds = val_ds.prefetch(buffer_size=32)
+
+## l'architecture du réseau !
+def make_model(input_shape, num_classes):
+    inputs = keras.Input(shape=input_shape)
+    # Image augmentation block
+    x = data_augmentation(inputs)
+
+    # Entry block
+    x = layers.experimental.preprocessing.Rescaling(1.0 / 255)(x)
+    x = layers.Conv2D(32, 3, strides=2, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    x = layers.Conv2D(64, 3, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    previous_block_activation = x  # Set aside residual
+
+    for size in [128, 256, 512, 728]:
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+        # Project residual
+        residual = layers.Conv2D(size, 1, strides=2, padding="same")(
+            previous_block_activation
+        )
+        x = layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
+    x = layers.SeparableConv2D(1024, 3, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    x = layers.GlobalAveragePooling2D()(x)
+    if num_classes == 2:
+        activation = "sigmoid"
+        units = 1
+    else:
+        activation = "softmax"
+        units = num_classes
+
+    x = layers.Dropout(0.5)(x)
+    outputs = layers.Dense(units, activation=activation)(x)
+    return keras.Model(inputs, outputs)
+
+
+model = make_model(input_shape=image_size + (3,), num_classes=2)
+model.summary()
+
+## QUESTION: Visualisation de l'architecture du réseau:
+# Installez pydot (pip install pydot) et graphviz (https://graphviz.gitlab.io/download/) 
+# pour visualiser le modèle, et exécutez la commande suivante:
+# keras.utils.plot_model(model, show_shapes=True)
+
+### Entrainement du modèle
+epochs = 50
+
+callbacks = [
+    keras.callbacks.ModelCheckpoint("save_at_{epoch}.h5"),
+]
+model.compile(
+    optimizer=keras.optimizers.Adam(1e-3),
+    loss="binary_crossentropy",
+    metrics=["accuracy"],
+)
+model.fit(
+    train_ds, epochs=epochs, callbacks=callbacks, validation_data=val_ds,
+)
+
+## Test sur une image
+img = keras.preprocessing.image.load_img(
+    "PetImages/Cat/6779.jpg", target_size=image_size
+)
+img_array = keras.preprocessing.image.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0)  # Create batch axis
+
+predictions = model.predict(img_array)
+score = predictions[0]
+print(
+    "This image is %.2f percent cat and %.2f percent dog."
+    % (100 * (1 - score), 100 * score)
+
